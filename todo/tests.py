ca5b8dc830afb0092c0649b1883460cc7cf3d0dc
1F10240022 <s1F102400224@iniad.org>
2025-07-03 13:38:26 +0900

    initial commit

--- todo/tests.py
+++ todo/tests.py
@@ -78,7 +78,7 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.templates[0].name, "todo/index.html")
         self.assertEqual(len(response.context["tasks"]), 1)
 
-    def test_index_get_order_post(self):
+    def test_index_get_order_post(self) -> None:
         task1: Task = Task(
             title="task1", due_at=timezone.make_aware(datetime(2024, 7, 1, 1))
         )
@@ -94,10 +94,14 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.context["tasks"][0], task2)
         self.assertEqual(response.context["tasks"][1], task1)
 
-    def test_index_get_order_due(self):
-        task1 = Task(title="task1", due_at=timezone.make_aware(datetime(2024, 7, 1)))
+    def test_index_get_order_due(self) -> None:
+        task1: Task = Task(
+            title="task1", due_at=timezone.make_aware(datetime(2024, 7, 1))
+        )
         task1.save()
-        task2 = Task(title="task2", due_at=timezone.make_aware(datetime(2024, 8, 1)))
+        task2: Task = Task(
+            title="task2", due_at=timezone.make_aware(datetime(2024, 8, 1))
+        )
         task2.save()
         client = Client()
         response = client.get("/?order=due")
--- todo/views.py
+++ todo/views.py
@@ -3,22 +3,21 @@ from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
 import django.http
-import django
 
 
 # Create your views here.
 def index(request: django.http.HttpRequest) -> django.http.HttpResponse:
     if request.method == "POST":
-        due = parse_datetime(request.POST.get("due_at", ""))
-        task = Task(
+        due: timezone.datetime | None = parse_datetime(request.POST.get("due_at", ""))
+        task: Task = Task(
             title=request.POST["title"],
             due_at=make_aware(due) if due else None,
         )
         task.save()
 
     if request.GET.get("order") == "due":
-        tasks = Task.objects.order_by("due_at")
+        tasks: list[Task] = Task.objects.order_by("due_at")
     else:
-        tasks = Task.objects.order_by("-created_at")
-    context = {"tasks": tasks}
+        tasks: list[Task] = Task.objects.order_by("-posted_at")
+    context: dict[str, list[Task]] = {"tasks": tasks}
     return render(request, "todo/index.html", context)
