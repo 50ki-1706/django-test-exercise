f639d01b9f8623aa1226c905a5888f55eb594875
1F10240022 <s1F102400224@iniad.org>
2025-07-10 14:31:09 +0900

    fix-deploy-settings

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
-
 pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-
+python manage.py collectstatic
+--no-input
 python manage.py migrate
-
-# Start the Django application with Gunicorn and Uvicorn worker
-exec python -m gunicorn config.asgi:application -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
